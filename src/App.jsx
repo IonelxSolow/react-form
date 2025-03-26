20c72e5304644de4e7a990b5cfd2acc54d4289f9
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-03-26 16:11:35 +0100

    Add article list component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import React from "react";
+import ArticlesList from "./components/ArticleList";
 
 function App() {
   return (
     <>
-      <h1>react-form</h1>
+      <h1>React-form</h1>
+      <div className="container mt-4">
+        <h2 className="mb-3">Lista Articoli</h2>
+        <ul className="list-group">
+          {ArticlesList.map((article) => (
+            <li key={article.id} className="list-group-item">
+              {article.title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,8 @@
+const articles = [
+  { id: 1, title: "Introduzione a React" },
+  { id: 2, title: "Guida a Vite e React" },
+  { id: 3, title: "Utilizzare Bootstrap con React" },
+  { id: 4, title: "Componenti e Props in React" },
+];
+
+export default articles;
